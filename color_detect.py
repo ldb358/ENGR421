d73880c990ee228b92ec01e2c5a2d283d0a1fe7c
ldb358 <ldb358@gmail.com>
2014-04-24 13:16:07 -0700

    finished the x line projection function now i need to test the percentage

--- color_detect.py
+++ color_detect.py
@@ -69,22 +69,120 @@ def find_arena_edge(frame):
     cv2.imshow("test", frame)
 
 
+def thresh_delta(borders, field, y):
+    dr, dl = 0, 0
+    xsize = borders.shape[1] / 2
+    for delta in range(xsize-1):
+        if field[y][xsize + delta] != 0 and dr == 0:
+            dr = delta
+        if field[y][xsize - delta] != 0 and dl == 0:
+            dl = delta
+
+    return xsize - dl, xsize + dr
+
+
+def extract_lines_old(borders):
+    b = None
+    start = 0
+    for i in range(50):
+        b = thresh_delta(borders, borders, i)
+        if b[0] != 0:
+            start = i
+            break
+    close = thresh_delta(borders, borders, 100)
+    far = thresh_delta(borders, borders, 400)
+    print "b", b
+    print "close", close
+    print "far", far
+    return b, close, far, start
+
+"""
+" Takes in varibles about a pucks location and the field and
+" initx: is the x of bottom left corner
+" fl: the rate of change of f(y) = fl*x+initx on the left side
+" fr: the same but the right side
+" delta_x: the width of the base of the board in pixels
+" projx, projy: the x and y you want to chnage to the real y
+"""
+def calc_real_y(initx, fl, fr, delta_x, projx, projy):
+    right_pos = fr(projy)
+    left_pos = fl(projy)
+    delta = right_pos-left_pos
+    print "y", projy, "right", right_pos, "left", left_pos
+    #get the percent of the total board
+    percent = float(projx-left_pos)/delta
+    print "projx", projx
+    print "delta", delta
+    return percent*delta_x+initx
+
+
+def draw_corners(im, corners):
+    cv2.line(im, tuple(corners[0]), tuple(corners[1]), (0, 255, 255), 10)
+    cv2.line(im, tuple(corners[1]), tuple(corners[3]), (0, 255, 0), 10)
+    cv2.line(im, tuple(corners[3]), tuple(corners[2]), (255, 255, 0), 10)
+    cv2.line(im, tuple(corners[2]), tuple(corners[0]), (100, 255, 100), 10)
+
+
 def main():
     #video = cv2.VideoCapture(1)
     video = cv2.VideoCapture("capture.avi")
+    #video = cv2.VideoCapture("test.avi")
     #grab a frame for calibration
     _, frame = video.read()
     #find_arena_edge(frame)
     im = frame.copy()
     imgray = cv2.cvtColor(im,cv2.COLOR_BGR2GRAY)
     imgray = cv2.blur(imgray, (10, 10))
-    ret, thresh = cv2.threshold(imgray, 127, 255, 0)
-    contours, hierarchy = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+
+    thresh = cv2.Canny(imgray, 100, 100, apertureSize=3)
+    lines = cv2.HoughLinesP(thresh, 1, math.pi/180, 70, minLineLength=30, maxLineGap=50)
+
+    borders = np.zeros(thresh.shape, np.uint8)
+    max_dy = [[0, lines[0][1]], [0, lines[0][1]]]
+    for line in lines[0]:
+        p1 = tuple(line[:2])
+        p2 = tuple(line[2:])
+        cv2.line(borders, p1, p2, (255, 0, 0), 5)
+        dy = abs(p2[1] - p1[1])
+        if dy > max_dy[0][0]:
+            if abs(max_dy[0][1][1] - p1[1]) > 100:
+                max_dy[1] = max_dy[0]
+            max_dy[0] = [dy, line]
+
+    delta = [abs(max_dy[0][1][0]-max_dy[1][1][2]), abs(max_dy[0][1][2]-max_dy[1][1][0])]
+    cv2.line(im, tuple(max_dy[0][1][:2]), tuple(max_dy[0][1][2:]), (255, 0, 0), 5)
+    cv2.line(im, tuple(max_dy[1][1][:2]), tuple(max_dy[1][1][2:]), (255, 0, 0), 5)
+    #contours, hierarchy = cv2.findContours(borders, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    cv2.imshow("test", borders)
+
+    f_r = (delta[0]-delta[1])/(max_dy[0][1][0]-max_dy[0][1][2])
+    #top-left, top-right, bottom-left, bottom-right,
+    corners = np.float32([
+        [max_dy[0][1][2], max_dy[0][1][3]],
+        [max_dy[0][1][2]+delta[1]+max_dy[0][1][3]*f_r, max_dy[0][1][3]],
+        [max_dy[0][1][0], max_dy[0][1][1]],
+        [max_dy[1][1][2], max_dy[1][1][3]],
+
+    ])
+    mr = (corners[3][1] - corners[1][1])/(corners[3][0] - corners[1][0])
+    br = corners[3][1] - corners[3][0]*mr
+    fr = lambda x: (x-br)/mr
+    ml = float(corners[0][0]-corners[2][0])/float(corners[0][1]-corners[2][1])
+    bl = corners[2][1] - corners[2][0]*ml
+    fl = lambda y: (y-bl)/ml
+    print "fr", fr(25)
+    print corners
+    print calc_real_y(corners[2][0], fl, fr, delta[0], corners[1][0], corners[1][1])
+    draw_corners(im, corners)
+
+    #keep old contours
+    """
     for cnt in contours:
         x,y,w,h = cv2.boundingRect(cnt)
         cv2.rectangle(im,(x,y),(x+w,y+h),(0,255,0),2)
+    """
     cv2.imshow("cont", im)
-
+    cv2.waitKey(0)
     while 1:
         _, frame = video.read()
         #generate the threshold images
@@ -105,6 +203,9 @@ def main():
             cv2.line(blue_raw, (rx, red[1]), (rx, 0), (0, 0, 255), 5)
         except TypeError:
             pass
+
+        draw_corners(blue_raw, corners)
+
         cv2.imshow("detected", blue_raw)
         cv2.imshow("Blue", blue_thresh)
         cv2.imshow("Red", red_thresh)
